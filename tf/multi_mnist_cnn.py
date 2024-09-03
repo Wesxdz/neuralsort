0b0bdb91c0e4403fc46e84d265bf36d3f05c4343
Wesxdz <sirwesleybarlow@gmail.com>
2024-09-03 14:21:53 -0600

    Try with Keras

--- tf/multi_mnist_cnn.py
+++ tf/multi_mnist_cnn.py
@@ -1,90 +1,109 @@
 import tensorflow as tf
 
-def deepnn(l, x, final_dim = 1):
-  """deepnn builds the graph for a deep net for classifying digits.
-  Args:
-    x: an input tensor with the dimensions (N_examples, 784), where 784 is the
-    number of pixels in a standard MNIST image.
-  Returns:
-    A tuple (y, keep_prob). y is a tensor of shape (N_examples, 10), with values
-    equal to the logits of classifying the digit into one of 10 classes (the
-    digits 0-9). keep_prob is a scalar placeholder for the probability of
-    dropout.
-  """
-  # Reshape to use within a convolutional neural net.
-  # Last dimension is for "features" - there is only one here, since images are
-  # grayscale -- it would be 3 for an RGB image, 4 for RGBA, etc.
+# https://keras.io/examples/vision/mnist_convnet/
+def deepnn(l, x, final_dim=1):
+    inputs = tf.keras.Input(shape=(l * 28, 28, 1))
+
+    x = tf.keras.layers.Conv2D(32, kernel_size=(3, 3), activation="relu")(inputs)
+    x = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(x)
+
+    x = tf.keras.layers.Conv2D(64, kernel_size=(3, 3), activation="relu")(x)
+    x = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(x)
+
+    x = tf.keras.layers.Flatten()(x)
+    x = tf.keras.layers.Dense(64, activation="relu")(x)
+    x = tf.keras.layers.Dropout(0.5)(x)
+
+    outputs = tf.keras.layers.Dense(final_dim)(x)
+
+    model = tf.keras.Model(inputs=inputs, outputs=outputs)
+    return model
+
+# def deepnn(l, x, final_dim = 1):
+#   """deepnn builds the graph for a deep net for classifying digits.
+#   Args:
+#     x: an input tensor with the dimensions (N_examples, 784), where 784 is the
+#     number of pixels in a standard MNIST image.
+#   Returns:
+#     A tuple (y, keep_prob). y is a tensor of shape (N_examples, 10), with values
+#     equal to the logits of classifying the digit into one of 10 classes (the
+#     digits 0-9). keep_prob is a scalar placeholder for the probability of
+#     dropout.
+#   """
+#   # Reshape to use within a convolutional neural net.
+#   # Last dimension is for "features" - there is only one here, since images are
+#   # grayscale -- it would be 3 for an RGB image, 4 for RGBA, etc.
 
 
-  with tf.compat.v1.name_scope('reshape'):
-    x_image = tf.reshape(x, [-1, l * 28, 28, 1])
+#   with tf.compat.v1.name_scope('reshape'):
+#     x_image = tf.reshape(x, [-1, l * 28, 28, 1])
 
-  # First convolutional layer - maps one grayscale image to 32 feature maps.
-  with tf.compat.v1.name_scope('conv1'):
-    W_conv1 = weight_variable([5, 5, 1, 32])
-    b_conv1 = bias_variable([32])
-    h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+#   # First convolutional layer - maps one grayscale image to 32 feature maps.
+#   with tf.compat.v1.name_scope('conv1'):
+#     W_conv1 = weight_variable([5, 5, 1, 32])
+#     b_conv1 = bias_variable([32])
+#     h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
 
-  # Pooling layer - downsamples by 2X.
-  with tf.compat.v1.name_scope('pool1'):
-    h_pool1 = max_pool_2x2(h_conv1)
+#   # Pooling layer - downsamples by 2X.
+#   with tf.compat.v1.name_scope('pool1'):
+#     h_pool1 = max_pool_2x2(h_conv1)
 
-  # Second convolutional layer -- maps 32 feature maps to 64.
-  with tf.compat.v1.name_scope('conv2'):
-    W_conv2 = weight_variable([5, 5, 32, 64])
-    b_conv2 = bias_variable([64])
-    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+#   # Second convolutional layer -- maps 32 feature maps to 64.
+#   with tf.compat.v1.name_scope('conv2'):
+#     W_conv2 = weight_variable([5, 5, 32, 64])
+#     b_conv2 = bias_variable([64])
+#     h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
 
-  # Second pooling layer.
-  with tf.compat.v1.name_scope('pool2'):
-    h_pool2 = max_pool_2x2(h_conv2)
+#   # Second pooling layer.
+#   with tf.compat.v1.name_scope('pool2'):
+#     h_pool2 = max_pool_2x2(h_conv2)
 
-  # Fully connected layer 1 -- after 2 round of downsampling, our 28x28 image
-  # is down to 7x7x64 feature maps -- maps this to 64 features.
-  with tf.compat.v1.name_scope('fc1'):
-    W_fc1 = weight_variable([l * 7 * 7 * 64, 64])
-    b_fc1 = bias_variable([64])
+#   # Fully connected layer 1 -- after 2 round of downsampling, our 28x28 image
+#   # is down to 7x7x64 feature maps -- maps this to 64 features.
+#   with tf.compat.v1.name_scope('fc1'):
+#     W_fc1 = weight_variable([l * 7 * 7 * 64, 64])
+#     b_fc1 = bias_variable([64])
 
-    h_pool2_flat = tf.reshape(h_pool2, [-1, l*7*7*64])
-    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+#     h_pool2_flat = tf.reshape(h_pool2, [-1, l*7*7*64])
+#     h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
-  with tf.compat.v1.name_scope('fc2'):
-    W_fc2 = weight_variable([64, final_dim])
-    b_fc2 = bias_variable([final_dim])
+#   with tf.compat.v1.name_scope('fc2'):
+#     W_fc2 = weight_variable([64, final_dim])
+#     b_fc2 = bias_variable([final_dim])
 
-    h_fc1_flat = tf.reshape(h_fc1, [-1, 64])
-    h_fc2 = tf.matmul(h_fc1_flat, W_fc2) + b_fc2
+#     h_fc1_flat = tf.reshape(h_fc1, [-1, 64])
+#     h_fc2 = tf.matmul(h_fc1_flat, W_fc2) + b_fc2
 
-  return h_fc2
+#   return h_fc2
 
-  # Dropout - controls the complexity of the model, prevents co-adaptation of
-  # features.
-  with tf.compat.v1.name_scope('dropout'):
-    keep_prob = tf.compat.v1.placeholder(tf.float32)
-    h_fc1_drop = tf.nn.dropout(h_fc1, rate=1 - (keep_prob))
+#   # Dropout - controls the complexity of the model, prevents co-adaptation of
+#   # features.
+#   with tf.compat.v1.name_scope('dropout'):
+#     keep_prob = tf.compat.v1.placeholder(tf.float32)
+#     h_fc1_drop = tf.nn.dropout(h_fc1, rate=1 - (keep_prob))
 
-  return h_fc1_drop, keep_prob
-
-
-def conv2d(x, W):
-  """conv2d returns a 2d convolution layer with full stride."""
-  return tf.nn.conv2d(x, filters=W, strides=[1, 1, 1, 1], padding='SAME')
+#   return h_fc1_drop, keep_prob
 
 
-def max_pool_2x2(x):
-  """max_pool_2x2 downsamples a feature map by 2X."""
-  return tf.nn.max_pool2d(input=x, ksize=[1, 2, 2, 1],
-                        strides=[1, 2, 2, 1], padding='SAME')
+# def conv2d(x, W):
+#   """conv2d returns a 2d convolution layer with full stride."""
+#   return tf.nn.conv2d(x, filters=W, strides=[1, 1, 1, 1], padding='SAME')
 
 
-def weight_variable(shape):
-  """weight_variable generates a weight variable of a given shape."""
-  initial = tf.random.truncated_normal(shape, stddev=0.1)
-  with tf.compat.v1.name_scope("reg"):
-    return tf.Variable(initial)
+# def max_pool_2x2(x):
+#   """max_pool_2x2 downsamples a feature map by 2X."""
+#   return tf.nn.max_pool2d(input=x, ksize=[1, 2, 2, 1],
+#                         strides=[1, 2, 2, 1], padding='SAME')
 
 
-def bias_variable(shape):
-  """bias_variable generates a bias variable of a given shape."""
-  initial = tf.constant(0.1, shape=shape)
-  return tf.Variable(initial)
+# def weight_variable(shape):
+#   """weight_variable generates a weight variable of a given shape."""
+#   initial = tf.random.truncated_normal(shape, stddev=0.1)
+#   with tf.compat.v1.name_scope("reg"):
+#     return tf.Variable(initial)
+
+
+# def bias_variable(shape):
+#   """bias_variable generates a bias variable of a given shape."""
+#   initial = tf.constant(0.1, shape=shape)
+#   return tf.Variable(initial)
